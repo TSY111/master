1b708f1efb9ae48410ea6b116c2a1c57e6d689c1
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-06-09 16:47:10 +1000

    Added some code to redownload Belgium data (and sdata) if not already downloaded.

--- contact_matrices.R
+++ contact_matrices.R
@@ -1,8 +1,23 @@
 rm(list = ls())
 
-be_survey <- get_survey("https://doi.org/10.5281/zenodo.4035001")
-saveRDS(be_survey, "be.rds")
-be_survey <- readRDS("be.rds")
+library(socialmixr)
+library(here)
+library(readr)
+
+## If Belgium data have not already been downloaded, redownload.
+if(!file.exists(here("data","be.rds")) || !file.exists(here("data","be_sday.csv"))){
+  be_URLs <- capture.output(be_survey <- get_survey("https://doi.org/10.5281/zenodo.4035001"), type="message")
+  sday_URL <-  be_URLs[min(grep("_sday.csv", be_URLs))] %>% sub("Downloading ", "", .)
+  dir.create(here("data"), FALSE)
+  download.file(sday_URL, here("data", "be_sday.csv"))
+  saveRDS(be_survey, here("data","be.rds"))
+}
+
+be_survey <- readRDS(here("data","be.rds"))
+be_sday <- read_csv(here("data","be_sday.csv"))
+
+
+
 
 
 data=be_survey
