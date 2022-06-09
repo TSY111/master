17c5665f3ae3928dcdb338d8c91685b375163436
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-06-09 17:31:58 +1000

    Rewrote contact distribution calculation and added a verison that includes non-contact participants.

--- contact_distribution.R
+++ contact_distribution.R
@@ -1,9 +1,20 @@
-conw1a=subset(conw1,part_age>18&part_age<30)
+library(here)
+source(here("get_data.R"))
 
-conw1partid=conw1a$part_id
+library(socialmixr)
 
-test=duplicated(conw1partid)
-connum=table(conw1partid[test])
-as.data.frame(connum)
+sday <- be_sday
+part <- be_part
+con <- be_con
 
-describe(connum)
+library(dplyr)
+partw1 <- inner_join(sday, part, by="part_id") %>% filter(wave==1)
+conw1 <- inner_join(sday, con, by="part_id") %>% filter(wave==1)
+
+connum_active <- conw1 %>% group_by(part_id) %>% summarise(ncontacts = n())
+table(connum_active$ncontacts)
+summary(connum_active)
+
+connum_all <- left_join(partw1, conw1, "part_id") %>% group_by(part_id) %>% summarise(ncontacts = sum(!is.na(sday_id.y)))
+table(connum_all$ncontacts)
+summary(connum_all)
