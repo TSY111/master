10976884fdad202e54fe0e3d958a97a724dc5b31
TSY111 <106672651+TSY111@users.noreply.github.com>
2022-07-17 01:48:11 +1000

    Update contact_distribution(by age group).R

--- contact_distribution(by age group).R
+++ contact_distribution(by age group).R
@@ -2,8 +2,8 @@
 options(digits = 2)	
 connum_waveIQR <- inner_join(sday, part, by="part_id") %>%
   left_join(con, by="part_id") %>%
-  group_by(wave, part_age, part_id) %>%
-  filter(part_age!="Under 18")%>%
+  group_by(wave, part_age_group, part_id) %>%
+  filter(part_age_group!="Under 18")%>%
   summarise(ncontacts = sum(!is.na(cont_id))) %>%
   summarise(mean = mean(ncontacts), Quartile.1 = quantile(ncontacts,0.25), Quartile.3 = quantile(ncontacts,0.75))
 #install.packages("flextable")
